89febd0fc0a9a4438855516df56c66d05be883f2
boxitdept <boxitdept@gmail.com>
2025-11-07 16:01:52 -0600

    Switch to Entra ID OAuth2 (azure_ad_oauth2) and set admin/transfer salts

--- config/admin.js
+++ config/admin.js
@@ -1,31 +1,47 @@
+const AzureAdOAuth2Strategy = require('passport-azure-ad-oauth2').Strategy;
+const jwt = require('jsonwebtoken');
+
 module.exports = ({ env }) => ({
-  apiToken: { salt: env('ADMIN_API_TOKEN_SALT') },
+  apiToken: {
+    salt: env('ADMIN_API_TOKEN_SALT'),         // fixes deprecation warning
+  },
+  transfer: {
+    token: { salt: env('ADMIN_TRANSFER_TOKEN_SALT') }, // fixes "Missing transfer.token.salt"
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
     providers: [
       {
-        uid: 'azuread',
-        displayName: 'Azure AD',
+        uid: 'azure_ad_oauth2',                // must be exactly this
+        displayName: 'Microsoft Entra ID',
         icon: 'https://learn.microsoft.com/favicon.ico',
-        createStrategy: (strapi) =>
-          new (require('passport-azure-ad').OIDCStrategy)(
+        createStrategy: () =>
+          new AzureAdOAuth2Strategy(
             {
-              identityMetadata: `https://login.microsoftonline.com/${env('AZURE_TENANT_ID')}/v2.0/.well-known/openid-configuration`,
+              authorizationURL: `https://login.microsoftonline.com/${env('AZURE_TENANT_ID')}/oauth2/v2.0/authorize`,
+              tokenURL:        `https://login.microsoftonline.com/${env('AZURE_TENANT_ID')}/oauth2/v2.0/token`,
               clientID: env('AZURE_CLIENT_ID'),
               clientSecret: env('AZURE_CLIENT_SECRET'),
-              responseType: 'code',
-              responseMode: 'query',
-              redirectUrl: `${env('ADMIN_URL')}/connect/azuread/callback`,
-              allowHttpForRedirectUrl: false,
+              callbackURL: `${env('ADMIN_URL')}/connect/azure_ad_oauth2/callback`, // path matches uid
               scope: ['openid', 'profile', 'email'],
-              validateIssuer: true,
-              passReqToCallback: true,
+              passReqToCallback: false,
             },
-            (req, iss, sub, profile, accessToken, refreshToken, done) => {
-              done(null, {
-                email: profile._json.email || profile._json.preferred_username,
-                username: profile._json.preferred_username,
-              });
+            (accessToken, refreshToken, params, _profile, done) => {
+              try {
+                const claims = params.id_token ? (jwt.decode(params.id_token) || {}) : {};
+                const email =
+                  claims.email ||
+                  claims.preferred_username ||
+                  claims.upn ||
+                  null;
+
+                if (!email) return done(new Error('Entra ID did not include an email/UPN claim'));
+
+                const username = claims.preferred_username || claims.name || email;
+                return done(null, { email, username });
+              } catch (e) {
+                return done(e);
+              }
             }
           ),
       },
